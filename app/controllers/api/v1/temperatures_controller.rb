1ca2bdc1534b19a975378c83e6c8c330641048a8
Yo_IT <yminghan@gmail.com>
2017-04-27 10:51:26 +0900

    add insert action into temperature controller

--- app/controllers/api/v1/temperatures_controller.rb
+++ app/controllers/api/v1/temperatures_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class TemperaturesController < ApplicationController
+      protect_from_forgery with: :null_session
+
       def index
         @temperatures = Temperature.where('place = ?', user_params[:place]).order(created_at: :desc).limit(60)
         @temperatures = @temperatures.map do |temperature|
@@ -16,10 +18,23 @@ module Api
         render json: @temperatures
       end
 
+      def insert
+        @record = Temperature.new
+        @record.place = user_params[:place]
+        @record.temperature = user_params[:temperature]
+        @record.humidity = user_params[:humidity]
+        @record.pressure = user_params[:pressure]
+        if @record.save!
+          render  json: @record
+        else
+
+        end
+      end
+
       private
 
       def user_params
-        params.permit(:place)
+        params.permit(:place, :temperature, :humidity, :pressure)
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :temperatures do
         get "/" , :action => "index"
+        post "/insert", :action => "insert"
       end
     end
   end
